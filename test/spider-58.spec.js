5e8a114c3d8da75c5c8de59b07af5ea26a4495d3
rockdragon <moyerock@gmail.com>
2014-11-18 12:00:22 +0800

    add test-travis

--- test/spider-58.spec.js
+++ test/spider-58.spec.js
@@ -1,14 +1,16 @@
 var list = require('../spiders/58/list');
 var run = require('../modules/es6/container').run;
+var expect = require('expect.js');
+require('should');
 
 describe('58 testing', function () {
     it('list testing', function (done) {
         run(function*(cb) {
             var listPage = yield list.getHouses()(cb);
-            expect(listPage).not.toBeNull();
-            expect(listPage.houses).not.toBeNull();
-            expect(listPage.houses).toEqual(jasmine.any(Array));
-            expect(listPage.houses.length).toBeGreaterThan(10);
+            expect(listPage).not.to.be.null;
+            expect(listPage.houses).not.to.be.null;
+            Object.prototype.toString.call(listPage.houses).should.equal('[object Array]');
+            listPage.houses.length.should.greaterThan(10);
             done();
         });
     });
