8b67daca1c50f4fe62b3cd47a83983e8bc44a589
rockdragon <moyerock@gmail.com>
2014-11-20 14:18:06 +0800

    upgrade node version to 0.11.14.

--- test/spider-58.spec.js
+++ test/spider-58.spec.js
@@ -1,12 +1,13 @@
 var list = require('../spiders/58/list');
 var run = require('../modules/es6/container').run;
+var co = require('co');
 var expect = require('expect.js');
 require('should');
 
 describe('58 testing', function () {
     it('list testing', function (done) {
-        run(function*(cb) {
-            var listPage = yield list.getHouses()(cb);
+        co(function*() {
+            var listPage = yield list.getHouses();
             expect(listPage).not.to.be(null);
             expect(listPage.houses).not.to.be(null);
             Object.prototype.toString.call(listPage.houses).should.equal('[object Array]');
