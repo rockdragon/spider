a221b1e063bd10d53e351d6e0422af0a47917a1c
Locke <moyerock@gmail.com>
2014-11-28 20:23:23 +0800

    re-construct model.

--- spiders/model.js
+++ spiders/model.js
@@ -4,18 +4,10 @@ module.exports.listPage = listPage;
  房模型
  @opt:{
  id             hash('5820038458323847x' or 'fang3/1268317789x')
- province       '北京'
- city           '北京'
- district       '朝阳'
- bizDistrict    '望京'
- building       '丽都水岸'
+ zone           '北京 - 昌平 - 沙河镇'
  address        '东风北桥北，酒仙桥路甲12号'
- houseType      '3室2厅'
- buildingType   '普通住宅' '公寓' '别墅'
- area           '180㎡'
- floor          '15/23层'
- orientation    '朝南北'
- fitment        '精装修'
+ overview       '1室1厅2卫 76㎡ 商住两用 精装修 朝向西 2层/3层'
+ furniture      '床 / 家具 / 煤气 / 暖气 / 宽带 / 电视 / 冰箱 / 洗衣机 / 热水器'
  phone          '15801429451'
  phonePic       'http://bj.ganji.com/tel_img/?c=UdxKaqrVYooZg055sS0ramfjr968g__PtQyV'
  title          '【21世纪官方推荐】3室2厅125平 出租,南北通透-整租'
@@ -25,28 +17,20 @@ module.exports.listPage = listPage;
  contact        '郑小姐'
  thumbnail      'http://pic4.58cdn.com.cn/p1/tiny/n_s02512217763172548164.jpg'
  href           'http://bj.58.com/hezu/19996422443272x.shtml'
- category       '[整租]' '[单间]'
- convenience    '距10号线芍药居站约392米'
  source         'ganji' '58' 'anjuke' 'soufun'
  publishDate    '2014/11/15'
+ description    '...'
  }
  */
 function house(opt) {
     opt = opt || {};
     if (opt.id)this.id = opt.id;
-    if (opt.province)this.province = opt.province;
-    if (opt.city)this.city = opt.city;
-    if (opt.district)this.district = opt.district;
-    if (opt.bizDistrict)this.bizDistrict = opt.bizDistrict;
-    if (opt.building)this.building = opt.building;
+    if (opt.zone)this.zone = opt.zone;
     if (opt.address)this.address = opt.address;
-    if (opt.houseType)this.houseType = opt.houseType;
-    if (opt.buildingType)this.buildingType = opt.buildingType;
-    if (opt.area)this.area = opt.area;
-    if (opt.floor)this.floor = opt.floor;
-    if (opt.orientation)this.orientation = opt.orientation;
-    if (opt.fitment)this.fitment = opt.fitment;
+    if (opt.overview)this.overview = opt.overview;
+    if (opt.furniture)this.furniture = opt.furniture;
     if (opt.phone)this.phone = opt.phone;
+    if (opt.phonePic)this.phonePic = opt.phonePic;
     if (opt.title)this.title = opt.title;
     if (opt.price)this.price = opt.price;
     if (opt.payment)this.payment = opt.payment;
@@ -54,8 +38,6 @@ function house(opt) {
     if (opt.contact)this.contact = opt.contact;
     if (opt.thumbnail)this.thumbnail = opt.thumbnail;
     if (opt.href)this.href = opt.href;
-    if (opt.category)this.category = opt.category;
-    if (opt.convenience)this.convenience = opt.convenience;
     if (opt.source)this.source = opt.source;
     if (opt.publishDate)this.publishDate = opt.publishDate;
 }
@@ -65,18 +47,10 @@ var Sequelize = require('sequelize')
 
 var House = sequelize.define('house', {
     id          : {type: Sequelize.STRING, primaryKey: true, unique: true},
-    province    : {type: Sequelize.STRING},
-    city        : {type: Sequelize.STRING},
-    district    : {type: Sequelize.STRING},
-    bizDistrict : {type: Sequelize.STRING},
-    building    : {type: Sequelize.STRING},
+    zone        : {type: Sequelize.STRING},
     address     : {type: Sequelize.STRING},
-    houseType   : {type: Sequelize.STRING},
-    buildingType: {type: Sequelize.STRING},
-    area        : {type: Sequelize.STRING},
-    floor       : {type: Sequelize.STRING},
-    orientation : {type: Sequelize.STRING},
-    fitment     : {type: Sequelize.STRING},
+    overview    : {type: Sequelize.STRING},
+    furniture   : {type: Sequelize.STRING},
     phone       : {type: Sequelize.STRING},
     phonePic    : {type: Sequelize.BLOB},
     title       : {type: Sequelize.STRING, allowNull: false},
@@ -86,8 +60,6 @@ var House = sequelize.define('house', {
     contact     : {type: Sequelize.STRING},
     thumbnail   : {type: Sequelize.BLOB},
     href        : {type: Sequelize.STRING, allowNull: false},
-    category    : {type: Sequelize.STRING},
-    convenience : {type: Sequelize.STRING},
     source      : {type: Sequelize.STRING},
     publishDate : {type: Sequelize.DATE, allowNull: false}
 });
