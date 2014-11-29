e8127c5416b260b82d6d5df92cca4d94cccfbadc
Locke <moyerock@gmail.com>
2014-11-29 12:38:51 +0800

    introduced Bravo package for regular expression parsing.

--- spiders/58/detail.js
+++ spiders/58/detail.js
@@ -8,6 +8,9 @@ var resolve = require('url').resolve;
 var getRootURL = require('../../modules/other/pathUtils').getRootURL;
 var extractImgSrc = require('../biz').extractImgSrc;
 var ent = require('ent');
+var bravo = require('bravo');
+var path = require('path');
+
 /*
  详情
  * */
@@ -34,31 +37,33 @@ function parse(fn) {
             normalizeWhitespace: true,
             xmlMode: true
         });
+
         //collect page info
-        var house = {};
-        house.title = $('div.bigtitle h1').text().trim();
-        var $time = $('li.time');
-        if ($time) {
-            var $timeScript = $time.children();
-            if ($timeScript.length > 0) {
-                house.publishDate = new Date();
-            } else {
-                house.publishDate = $time.text().trim();
-            }
-        }
-        var $price = $('span.bigpri');
-        house.price = $price.text().trim();
-        house.payment = $price.next().text().trim();
-        house.overview = ent.decode($('div.su_tit:contains("概况")').next().text()).trim();
-        var $zone = $('div.su_tit:contains("区域")').next();
-        if ($zone.children() && $zone.children().length > 0) {
-            var zoneText = ent.decode($zone.text());
-            house.zone = new RegExp(/(\S+\s-\s\S+\s-\s\S+).?/g).exec(zoneText)[1];
-        } else {
-            house.zone = $zone.text() + ' - ' + $zone.next().text() + ' - ' + $zone.next().next().text();
-        }
-        var $address = $('div.su_tit:contains("地址")').next();
-        house.address = new RegExp(/([^\s]+)/g).exec($address.text())[1];
+        var jsonFile = path.join(process.cwd(), 'detail.json');
+        var house = bravo.Parse(jsonFile, res);
+
+        //var $time = $('li.time');
+        //if ($time) {
+        //    var $timeScript = $time.children();
+        //    if ($timeScript.length > 0) {
+        //        house.publishDate = new Date();
+        //    } else {
+        //        house.publishDate = $time.text().trim();
+        //    }
+        //}
+        //var $price = $('span.bigpri');
+        //house.price = $price.text().trim();
+        //house.payment = $price.next().text().trim();
+        //house.overview = ent.decode($('div.su_tit:contains("概况")').next().text()).trim();
+        //var $zone = $('div.su_tit:contains("区域")').next();
+        //if ($zone.children() && $zone.children().length > 0) {
+        //    var zoneText = ent.decode($zone.text());
+        //    house.zone = new RegExp(/(\S+\s-\s\S+\s-\s\S+).?/g).exec(zoneText)[1];
+        //} else {
+        //    house.zone = $zone.text() + ' - ' + $zone.next().text() + ' - ' + $zone.next().next().text();
+        //}
+        //var $address = $('div.su_tit:contains("地址")').next();
+        //house.address = new RegExp(/([^\s]+)/g).exec($address.text())[1];
         fn(err, house);
     };
 }
