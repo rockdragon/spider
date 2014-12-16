26b9a218e47882ea6264d704804ec2d9be4c54af
rockdragon <moyerock@gmail.com>
2014-12-16 14:56:02 +0800

    integrate with adminlte.

--- web/www.js
+++ web/www.js
@@ -1,10 +1,12 @@
 var koa = require('koa');
 var mount = require('koa-mount');
 var render = require('koa-ejs');
+var serve = require('koa-static');
 var getAbsolutePath = require('../modules/other/pathUtils').getAbsolutePath;
 
 //settings
 var app = koa();
+app.use(serve(getAbsolutePath('bower_components')));
 render(app, {
     root: getAbsolutePath('web/views'),
     layout: false,
@@ -16,8 +18,8 @@ render(app, {
 //routes
 var index = require(getAbsolutePath('web/routes/index'));
 var data = require(getAbsolutePath('web/routes/data'));
-app.use(mount('/', index));
 app.use(mount('/data', data));
+app.use(mount('/admin', index));
 
 //listen
 app.listen(3000);
